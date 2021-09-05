a0ad044990fcc4313d9a70c5963e8e19fefca7b2
hunterD3CD <hunterd3c@gmail.com>
2021-09-04 19:33:51 -0700

    create controller - user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,63 +1,95 @@
-const { Pizza } = require('../models');
+const { User } = require("../models");
 
-const pizzaController = {
-  // get all pizzas
-  getAllPizza(req, res) {
-    Pizza.find({})
+const userController = {
+  // ---------------------------------------------------------------/api/users-------------------------------------------------------------
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
       .populate({
-        path: 'comments',
-        select: '-__v'
+        path: "thoughts",
+        select: "-__v",
       })
-      .select('-__v')
+      .select("-__v")
       .sort({ _id: -1 })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
         console.log(err);
         res.sendStatus(400);
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
+  // get one User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
       .populate({
-        path: 'comments',
-        select: '-__v'
+        path: "thoughts",
+        select: "-__v",
       })
-      .select('-__v')
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
         console.log(err);
         res.sendStatus(400);
       });
   },
 
-  // createPizza
-  createPizza({ body }, res) {
-    Pizza.create(body)
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.json(err));
+  // createUser
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
   },
 
-  // update pizza by id
-  updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+  // update User by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
           return;
         }
-        res.json(dbPizzaData);
+        res.json(dbUserData);
       })
-      .catch(err => res.json(err));
+      .catch((err) => res.json(err));
   },
 
-  // delete pizza
-  deletePizza({ params }, res) {
-    Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.json(err));
-  }
+  // delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  // ---------------------------------------------------------------/api/users/:userId/friends/:friendId-------------------------------------------------------------
+  // add friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $addToSet: { friends: params.friendsId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+  // remove friend
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendsId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "no user found with this ID" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
-module.exports = pizzaController;
+module.exports = userController;
